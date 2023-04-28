88eca63df08e6eea7f8c29cb0bb77b8a141bd51f
Lehmons <freddiejackson217@gmail.com>
2023-04-28 16:40:49 +0100

    components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import HeaderStyles from "./Header.styled";
-import Image from 'next/image';
+import Image from "next/image";
 import logo from "../../../public/images/logo.svg";
+import hamburger from "../../../public/images/hamburger.svg";
+import search from "../../../public/images/search.svg";
+import myFT from "../../../public/images/myFT.svg";
 
 export default function Header() {
   return (
     <HeaderStyles>
-			<Image
-				priority
-				src={logo}
-				alt="Financial Times"
-				className="logo"
-			/>
+      <Image priority src={logo} alt="Financial Times" className="logo" />
+      <div class="nav-column">
+        <Image
+          priority
+          src={hamburger}
+          alt="Financial Times"
+          className="hamburger"
+        />
+        <Image priority src={search} alt="Search" className="search" />
+      </div>
+      <Image priority src={myFT} alt="My Financial Times" className="myFT" />
     </HeaderStyles>
   );
 }
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -6,20 +6,66 @@ import styled from "styled-components";
 import { font, media, underline } from "../Styles";
 
 const HeaderStyles = styled.header`
-  position: fixed;
-	width: 100%;
-	height: 6rem;
-	background: ${props => props.theme.pink100};
-	${media.smallDesktopAndBelow`
-		height: 60px;
+  display: flex;
+  top: 0;
+  left: 0;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 8.8rem;
+  background: ${(props) => props.theme.pink100};
+  ${media.smallDesktopAndBelow`
+		height: 88px;
 	`}
 
-	.logo {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-	}
+  .logo {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 50rem;
+    ${media.smallDesktopAndBelow`
+		max-width: 250px;
+    `}
+    ${media.tabletPortraitAndBelow`
+		max-width: 200px;
+    `}
+  }
+
+  svg {
+    position: relative;
+  }
+
+  .nav-column {
+    margin-left: 1rem;
+    ${media.smallDesktopAndBelow`
+      margin-left: 10px;
+    `}
+  }
+
+  .hamburger .search {
+    display: flex;
+    position: flex-start;
+  }
+
+  .search {
+    margin-left: 2rem;
+    ${media.smallDesktopAndBelow`
+      margin-right: 20px;
+    `}
+    ${media.tabletPortraitAndBelow`
+      display: none;
+    `}
+  }
+
+  .myFT {
+    display: flex;
+    position: flex-end;
+    margin-right: 1rem;
+    ${media.smallDesktopAndBelow`
+      margin-right: 10px;
+    `}
+  }
 `;
 
 export default HeaderStyles;
